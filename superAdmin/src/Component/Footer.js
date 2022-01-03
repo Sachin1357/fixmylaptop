8ceeb6098a6767beff42bd0f063255db0d19935f
sachin <sachin@justgoweb.com>
2022-01-03 17:14:14 +0530

    Images added

--- superAdmin/src/Component/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <div className='footer text-left'>
-            <p>Copyright @ 2021 All rights reseved. </p>
-        </div>
-    )
-}
